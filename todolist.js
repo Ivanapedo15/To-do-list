eb552a76b5a077294f481f4822c03ab76cbefdf6
Ivanapedo15 <119987279+Ivanapedo15@users.noreply.github.com>
2024-10-05 00:16:52 +0000

    Basic styling for the to-do list interface
    Basic styling for the to-do list interface

--- todolist.js
+++ todolist.js
@@ -1,18 +1,23 @@
 const inputField = document.getElementById("input-field");
 const taskList = document.getElementById("task-list");
 
-function addToList(){
-    if (inputField.value === ''){
-        alert("why!!");
+
+inputField.addEventListener("keypress", function(event){
+    if (event.key === "Enter"){
+        addToList()
     }
-    else{
+})
+
+
+function addToList(){
+    if (inputField.value !== ''){
         let li = document.createElement("li");
         li.innerHTML = inputField.value;
         taskList.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
-    };
+    }
     inputField.value = "";
 }
 
